b50b3aff703d75d7fcd74cf07c45d015e26c93a5
Mario Henrique <92612777+Gasparzinho20@users.noreply.github.com>
2022-03-28 15:33:24 -0300

    Painel-Admin
    commi

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCZ5sU9vfIuHSOy7spyBUuZNPPCjkKP6rs",
+    authDomain: "cardapio-web-6e6c4.firebaseapp.com",
+    databaseURL: "https://cardapio-web-6e6c4-default-rtdb.firebaseio.com",
+    projectId: "cardapio-web-6e6c4",
+    storageBucket: "cardapio-web-6e6c4.appspot.com",
+    messagingSenderId: "745015567594",
+    appId: "1:745015567594:web:b49e430bd331915c0ba393"
+  }
+ };
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCZ5sU9vfIuHSOy7spyBUuZNPPCjkKP6rs",
+    authDomain: "cardapio-web-6e6c4.firebaseapp.com",
+    databaseURL: "https://cardapio-web-6e6c4-default-rtdb.firebaseio.com",
+    projectId: "cardapio-web-6e6c4",
+    storageBucket: "cardapio-web-6e6c4.appspot.com",
+    messagingSenderId: "745015567594",
+    appId: "1:745015567594:web:b49e430bd331915c0ba393"
+  }
+ };
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
